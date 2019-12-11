6e192abbe4e5d8777718a1efd8fa2eea27d3e78e
Fatima <fkeita@smith.edu>
2019-12-11 17:56:59 -0500

    revised functions in common_functions.R script to include "stata" in each function name

--- R/common_functions.R
+++ R/common_functions.R
@@ -4,35 +4,35 @@ library(tibble)
 library(mosaic)
 library(ggplot2)
 
-sum <- function(dataset){
+stata_sum <- function(dataset){
   return(summary(dataset, na.rm = TRUE))
 }
 
-sum_var <- function(x, dataset) {
+stata_sum_var <- function(x, dataset) {
   return(favstats(x, data = data))
 }
 
-describe <- function(dataset) {
+stata_desc <- function(dataset) {
   return(glimpse(dataset))
 }
 
-list <- function(dataset){
+stata_list <- function(dataset){
   return(View(dataset))
 }
 
-list_2 <- function(dataset){
+stata_list_2 <- function(dataset){
   return(View(head(dataset,2)))
 }
 
-list_10 <- function(dataset){
+stata_list_10 <- function(dataset){
   return(View(head(dataset,10)))
 }
 
-graph <- function(variable1,variable2, dataset){
+stata_graph <- function(variable1,variable2, dataset){
   return(ggplot(dataset, aes(x=variable2, y=variable1)) + geom_point())
 }
 
-reg <- function(y, x, data){
+stata_reg <- function(y, x, data){
   fit <- lm(y~x, data = data)
   return(fit)
 }
