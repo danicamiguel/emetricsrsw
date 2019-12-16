35606bf15af7195608e884e05396bb13a4af74c8
danicamiguel <dmiguel@smith.edu>
2019-12-16 15:03:47 -0500

    updating test

--- data-raw/data-raw.R
+++ data-raw/data-raw.R
@@ -1,9 +1,6 @@
 ## code to prepare `data-raw` dataset goes here
 
-usethis::use_data("data-raw")
-
-library(tidyverse)
-library(dplyr)
-library(readxl)
-
 caschool <- read_excel("caschool.xlsx")
+usethis::use_data("caschool")
+
+
--- tests/testthat/test-functions.R
+++ tests/testthat/test-functions.R
@@ -1,5 +1,5 @@
 test_that("functions correct", {
-  expect_equal(stata_sum(caschool),stata_sum(caschool))
+  expect_match(stata_sum(caschool), "Min.", all = FALSE)
   expect_equal(stata_sum_var(caschool$teachers,caschool),stata_sum_var(caschool$teachers,caschool))
   expect_identical(stata_desc(caschool),stata_desc(caschool))
   expect_identical(stata_list(caschool),stata_list(caschool))
