96381f73b51cecff4eb16131de1b2317bcf050f5
Lizette <lcarpenter@smith.edu>
2019-12-16 00:09:20 -0500

    added roxygen comments

--- R/common_functions.R
+++ R/common_functions.R
@@ -1,12 +1,12 @@
 #chapter5_functions 
 #Includes: 
-#    stata_sum: 
-#    stata_sum_var:
-#    stata_desc: 
-#    stata_list: 
-#    stata_list_2:
-#    stata_list_10:
-#    stata_graph:
+#    stata_sum: returns summary results of specified dataset. 
+#    stata_sum_var: returns summary results of a specified variable in a given dataset. 
+#    stata_desc: returns detailed information of the dataset including the first 10 values of each of the columns.
+#    stata_list: returns a new window containing a specified dataset. 
+#    stata_list_2: returns a new window containing the first two rows of a specified dataset. 
+#    stata_list_10: returns a new window containing the first ten rows of a specified dataset. 
+#    stata_graph: 
 #    stata_reg:
 #    stata_regcoeff:
 #    stata_logit:
@@ -17,8 +17,8 @@
 #' Summary results of dataset. 
 #' Produces result summaries of the various model fitting functions.  
 #' 
-#' @param dataset - List
-#' @return List containing the summary results of the passed argument.  
+#' @param dataset - list, dataset 
+#' @return list containing the summary results of the passed argument.  
 #' 
 #' @examples 
 #' stata_sum(caschool)
@@ -27,32 +27,118 @@
 #' \dontrun{
 #' stata_sum(caschool, mtcars)
 #' }
+#' @export
 stata_sum <- function(dataset){
   return(summary(dataset, na.rm = TRUE))
 }
 
+#' stata_sum_var 
+#' 
+#' Summary results of a specified variable in a given dataset.  
+#' Produces result summaries of the various model fitting functions.  
+#' 
+#' @param x - a numeric vector, variable from a datset 
+#' @param dataset - list, dataset 
+#' @return a vector of statistical summaries 
+#' 
+#' @examples 
+#' stata_sum_var(caschool$teachers, caschool)
+#' stata_sum_var(mtcars$cyl, mtcars)
+#' 
+#' \dontrun{
+#' stata_sum_var(teachers, caschool)
+#' }
 #'@import mosaic 
+#'@export 
 stata_sum_var <- function(x, dataset) {
   return(favstats(x, data = data))
 }
 
-#'@import tibble
+#' stata_desc 
+#' 
+#' Gives a glimpse of the dataset. 
+#' Provides detailed information of the dataset including the first 10 values of each of the columns.   
+#' 
+#' @param dataset - a data table 
+#' @return list of the dataset attributes including every column of the dataset 
+#'  
+#' @examples 
+#' stata_desc(caschool)
+#' stata_desc(mtcars)
+#' 
+#' \dontrun{
+#' stata_desc(caschool, mtcars)
+#' }
+#'@import tibble 
+#'@export 
 stata_desc <- function(dataset) {
   return(glimpse(dataset))
 }
 
+#' stata_list 
+#' 
+#' View dataset 
+#' Allows user to see a given dataset in a new window.   
+#' 
+#' @param dataset - an R object which can be coerced to a data frame
+#' @return a new window containing the dataset 
+#'  
+#' @examples 
+#' stata_list(caschool)
+#' stata_list(mtcars)
+#' 
+#' \dontrun{
+#' stata_list(caschool, mtcars)
+#' }
+#'@export 
 stata_list <- function(dataset){
   return(View(dataset))
 }
 
+#' stata_list_2 
+#' 
+#' View dataset 
+#' Allows user to see a the first two rows of a given dataset in a new window.   
+#' 
+#' @param dataset - an R object which can be coerced to a data frame
+#' @return a new window containing the first two rows of a given dataset 
+#'  
+#' @examples 
+#' stata_list_2(caschool)
+#' stata_list_2(mtcars)
+#' 
+#' \dontrun{
+#' stata_list_2(caschool, mtcars)
+#' }
+#'@export
 stata_list_2 <- function(dataset){
   return(View(head(dataset,2)))
 }
 
+#' stata_list_10 
+#' 
+#' View dataset 
+#' Allows user to see a the first ten rows of a given dataset in a new window.   
+#' 
+#' @param dataset - an R object which can be coerced to a data frame
+#' @return a new window containing the first ten rows of a given dataset 
+#'  
+#' @examples 
+#' stata_list_10(caschool)
+#' stata_list_10(mtcars)
+#' 
+#' \dontrun{
+#' stata_list_10(caschool, mtcars)
+#' }
+#'@export
 stata_list_10 <- function(dataset){
   return(View(head(dataset,10)))
 }
 
+#' stata_graph 
+#' 
+#' Plots a scatterplot. 
+#' 
 #' @import ggplot2
 stata_graph <- function(variable1,variable2, dataset){
   return(ggplot(dataset, aes(x=variable2, y=variable1)) + geom_point())
