cd82a6aae7f169691b0bc4c364b3c1a8e9c202fd
Lizette <lcarpenter@smith.edu>
2019-12-16 18:30:30 -0500

    added dataset with binary predictor

--- data-raw/data-raw.R
+++ data-raw/data-raw.R
@@ -2,6 +2,10 @@
 library(usethis)
 library(readxl)
 caschool <- read_excel("caschool.xlsx")
+
+bithweight_smoking <- read_excel("birthweight_smoking.xlsx")
+
 usethis::use_data(caschool, overwrite = TRUE)
+usethis::use_data(bithweight_smoking, overwrite = TRUE)
 
 
